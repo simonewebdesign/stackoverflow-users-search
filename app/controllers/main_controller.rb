d4a778c83e11079169cd99ac28de85f6e9afe182
Simone <hello@simonewebdesign.it>
2014-01-05 14:02:14 +0000

    Request and response

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,12 @@
 class MainController < ApplicationController
   def index
+    # TODO move in config file
+    url = "http://api.stackexchange.com/2.1/users?order=desc&sort=reputation&inname=simone&site=stackoverflow"
+    request = Typhoeus::Request.new("http://simonewebdesign.it/blog/")
+    request.run
+    response = request.response
+    puts response.inspect
+    # @json = JSON.parse response.body
   end
 
   def about
