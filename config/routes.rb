7b4e0c6331004a7c7d0c5d7ce631cce0fd308cb7
Simone <hello@simonewebdesign.it>
2014-01-05 13:25:29 +0000

    rails g controller main index about contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "main/index"
+
+  get "main/about"
+
+  get "main/contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
