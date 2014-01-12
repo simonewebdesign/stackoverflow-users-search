275f9f2f780fdcae66f558a790708cc269dbcb0d
Simone <hello@simonewebdesign.it>
2014-01-12 13:39:34 +0000

    Solve encoding problem

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,12 +1,20 @@
 class MainController < ApplicationController
   def index
     # TODO move in config file
-    url = "http://api.stackexchange.com/2.1/users?order=desc&sort=reputation&inname=simone&site=stackoverflow"
-    request = Typhoeus::Request.new("http://simonewebdesign.it/blog/")
+    url = "http://api.stackexchange.com/2.1/users"
+    request = Typhoeus::Request.new(
+      url, 
+      headers: { Accept: "application/json", accept_encoding: "gzip" }
+    )
     request.run
     response = request.response
-    puts response.inspect
+    p "RESPONSE HEADERS"
+    p response.headers_hash
     # @json = JSON.parse response.body
+    p "BODY"
+    p response.body
+    gz = Zlib::GzipReader.new(StringIO.new(response.body))    
+    @json = gz.read
   end
 
   def about
