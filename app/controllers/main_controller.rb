c932803087bfd2fd12158b547612d7c7455826c8
Simone <hello@simonewebdesign.it>
2014-01-12 23:19:12 +0000

    Display users (if any)

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,25 +1,40 @@
 class MainController < ApplicationController
   def index
     # TODO move in config file
-    url = "http://api.stackexchange.com/2.1/users"
-    request = Typhoeus::Request.new(
-      url, 
-      headers: { Accept: "application/json", accept_encoding: "gzip" }
-    )
-    request.run
-    response = request.response
-    p "RESPONSE HEADERS"
-    p response.headers_hash
-    # @json = JSON.parse response.body
-    p "BODY"
-    p response.body
-    gz = Zlib::GzipReader.new(StringIO.new(response.body))    
-    @json = gz.read
+    # url = "https://api.stackexchange.com/2.1/users?order=desc&sort=reputation&inname=simone&site=stackoverflow"
+    # request = Typhoeus::Request.new(
+    #   url, 
+    #   headers: { Accept: "application/json", accept_encoding: "gzip" }
+    # )
+    # request.run
+    # response = request.response
+    # gz = Zlib::GzipReader.new(StringIO.new(response.body))
+    # json = JSON.parse gz.read, symbolize_names: true
+    # # tentiamo di filtrare un po' i dati...
+    # @users = json[:items]
   end
 
   def about
   end
 
   def contact
+  end
+
+  def search
+    # TODO move in config file
+    url = "https://api.stackexchange.com/2.1/users?order=desc&sort=reputation" +
+          "&site=stackoverflow&inname=" + params[:user].to_s
+    request = Typhoeus::Request.new(
+      url, 
+      headers: { Accept: "application/json", accept_encoding: "gzip" }
+    )
+    request.run
+    response = request.response
+    gz = Zlib::GzipReader.new(StringIO.new(response.body))
+    json = JSON.parse gz.read, symbolize_names: true
+    # tentiamo di filtrare un po' i dati...
+    @users = json[:items] || []
+    p @users
+    render :index 
   end
 end
